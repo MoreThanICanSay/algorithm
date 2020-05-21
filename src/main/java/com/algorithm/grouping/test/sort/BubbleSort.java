bc173c1fb5c64b55f9bc291a980c562ff245bff0
zhangwenhui <zhangwenhui2018@163.com>
2020-05-21 23:26:57 +0800

    冒泡排序优化 添加无序列表的边界

--- src/main/java/com/algorithm/grouping/test/sort/BubbleSort.java
+++ src/main/java/com/algorithm/grouping/test/sort/BubbleSort.java
@@ -50,21 +50,44 @@ public class BubbleSort {
         }
     }
 
-//    public void sort3(int[] array) {
-//        // 记录最后一次交换的位置
-//        int lastExchangeIndex = 0;
-//        // 无序数列的边界，每次比较只要到这里为止
-//        int sortBorder = array.length - 1;
-//        for (int i = 0; i < array.length - 1; i++) {
-//            // 有序标记，每一轮的初始值都是true
-//
-//        }
-//    }
+    /**
+     * 添加无序列表的边界
+     *
+     * @param array
+     */
+    public static void sort3(int[] array) {
+        // 记录最后一次交换的位置
+        int lastExchangeIndex = 0;
+        // 无序数列的边界，每次比较只要到这里为止
+        int sortBorder = array.length - 1;
+        for (int i = 0; i < array.length - 1; i++) {
+            // 有序标记，每一轮的初始值都是true
+            boolean isSorted = true;
+            for (int j = 0; j < sortBorder; j++) {
+                int temp = 0;
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    // 有元素交换，所以不是有序的，标记为false
+                    isSorted = false;
+                    // 更新最后一次为交换元素的位置
+                    lastExchangeIndex = j;
+                }
+            }
+            sortBorder = lastExchangeIndex;
+            if (isSorted) {
+                break;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[] array = new int[]{5, 8, 3, 2, 5, 7, 8, 9, 1};
 //        sort1(array);
-        sort2(array);
+//        sort2(array);
+        sort3(array);
         System.out.println(Arrays.toString(array));
     }
 }
